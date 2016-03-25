68679195fc26d07f91843a0960d2bfd42ea01915
BellaT <bella_taylor@hotmail.co.uk>
2016-03-25 13:00:46 +0000

    got most of the html functioning and ready to start testing functions

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 //those will be selected at random from the beginning and every time player clicks on PLAY AGAIN
 //the player will choose 4 colours at a time from the selection printed on the screen
 // use CLICK event here to select colours to input into the first block, then 2nd, then 3rd, then 4th
+// Therefore you need to work out how when the player selects one of the colours, that colour is then moved into the first box etc.
 //PUSH each choice into an ARRAY called "PLAYERMOVE"
 // you will need to compare the array of colours the player has chosen to the array the computer has chosen
 // then calculate whether the player has chosen any of the right colours, and IF so are they in the right place?
@@ -13,3 +14,35 @@
 // ....ELSE return false? or null? or ""? (emply html)
 // IF the player guesses correctly within the 10 choices display that in some way - party gif popping up?
 // ELSE if they don't correct within the 10 choices they lose.
+
+$(function() {
+
+var computerArray = [];
+
+
+function colorSelect() {
+  var colors = ['red', 'blue', 'green', 'purple', 'yellow', 'orange'];
+  for (var i = 0; i < 4; i++) {
+    var selectedColor = colors[Math.floor(Math.random()*colors.length)];
+    computerArray.push(selectedColor);
+  }
+
+  console.log(computerArray);
+}
+
+colorSelect();
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
