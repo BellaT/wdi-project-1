c1bdc516abfc96990958ce69eada05fae5cca625
BellaT <bella_taylor@hotmail.co.uk>
2016-03-25 16:14:22 +0000

    have got the colour buttons to place in the right boxes

--- js/app.js
+++ js/app.js
@@ -17,10 +17,47 @@
 
 $(function() {
 
-var computerArray = [];
+  var computerArray   = [];
+  var playerSelection = [];
+  var $rows           = $(".singleRows");
+  var cells           = $(".cells");
+  var grid            = [];
+  var numberOfGos     = 0;
+  var numberOfGuesses = 0;
 
+  $(".colorChoice button").on("click", function(){
+    // Get the selected color
+    var color       = this.id;
+    // Find the row, according to the number of "gos" you've had (4xguesses)
+    var $rowToPlay  = $($rows[1 + numberOfGos]);
+    // Find the next cell to play, according to the number of guesses you've had
+    var $cellToPlay = $($rowToPlay.children(".cells")[1+numberOfGuesses])
+    // Change the cell to play to be the colour of the button that you have clicked
+    $cellToPlay.css("background", color);
+    playerSelection.push(color);
+    console.log(playerSelection);
+
+    // Increment the number of guesses
+    numberOfGuesses++;
+    if (numberOfGuesses === 4) {
+      numberOfGos++;
+      numberOfGuesses = 0;
+      // Check for matches
+      // Add the marker pins
+    }
+  })
+
+
+function start() {
+  //want to loop over the array of color choices and run a click event on them
+  // so then which ever one the player has clicked on an action will run
+  //PUSH each of their choices into a new array which will be compared to computers
+  // You also want to display the chosen color in the correct box! How!!!
+}
 
 function colorSelect() {
+  //this function is for the computer to select 4 colours out of the 6 at random
+  //then save them into a new array which will then by compared to player's choice
   var colors = ['red', 'blue', 'green', 'purple', 'yellow', 'orange'];
   for (var i = 0; i < 4; i++) {
     var selectedColor = colors[Math.floor(Math.random()*colors.length)];
@@ -39,10 +76,10 @@ colorSelect();
 
 
 
-
-
-
 });
+
+
+
 
 
 
